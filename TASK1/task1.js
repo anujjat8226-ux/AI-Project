8fe112fe6820dc4caaaa4a48a7755646645d3f16
anujjat8226-ux <anujjat8226@gmail.com>
2025-08-30 10:21:52 +0530

    Add files via upload

---
+++ TASK1/task1.js
@@ -0,0 +1,17 @@
+// Defining student object
+const student = {
+    name: "Anuj Jat",
+    age: 20,
+    rollNo: "0187AS241033",
+    course: "Bachelor of Technology",
+    branch: "CSE - AI & DS",
+    college: "Sagar Institute of Science and Technology"
+};
+
+// Printing details
+console.log("Name: " + student.name);
+console.log("Age: " + student.age);
+console.log("Roll No: " + student.rollNo);
+console.log("Course: " + student.course);
+console.log("Branch: " + student.branch);
+console.log("College: " + student.college);
